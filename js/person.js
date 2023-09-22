c8ccee6dcb3dd8d080f6902d3e669fe3153997b2
radclim <mmajaaaa@gmail.com>
2023-09-22 10:15:01 +0200

    person constructor, added 1 default person

---
+++ js/person.js
@@ -0,0 +1,13 @@
+class Person {
+    constructor(firstName, lastName, age) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this. age =  age;
+    }
+  }
+
+  const persons = [];
+
+  persons.push(new Person("Mathias", "Ulrich", 28));
+
+  console.log(persons);
