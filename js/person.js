51fe1e7c1ea674aabc64ea54eec1b7e8f523d14e
Walocial <walocial@hotmail.com>
2023-09-26 12:38:48 +0200

    xd

--- js/person.js
+++ js/person.js
@@ -12,6 +12,17 @@ const persons = [];
 
 //Adding default person
   persons.push(new Person("Mathias", "Ulrich", 28));
+  persons.push(new Person("Bo", "Lig", 14));
+  persons.push(new Person("Ib", "A", 19));
+  persons.push(new Person("Hans", "Grete", 89));
+  persons.push(new Person("Inger", "Iør", 67));
+  persons.push(new Person("Maja", "Smith", 42));
+  persons.push(new Person("Kost", "Eskab", 8));
+  persons.push(new Person("Camilla", "Ibsen", 99));
+  persons.push(new Person("Åge", "Låge", 75));
+  persons.push(new Person("Karl", "Ach", 120));
+  persons.push(new Person("Elena", "Anele", 50));
+
 
 //Fetching elements from HTML
 let firstNameInput = document.getElementById("firstName");
