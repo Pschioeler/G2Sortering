64652d8429b3962bb53a481db64f918f1707b83a
Walocial <walocial@hotmail.com>
2023-09-26 08:52:45 +0200

    Update filter.js

--- js/filter.js
+++ js/filter.js
@@ -2,6 +2,10 @@ const searchBar = document.getElementById('search');
 
 searchBar.onkeyup = () => {
     let userInput = searchBar.value.toLowerCase();
-    let test = persons.filter("??");
-    console.log(test);
+    let filteredPersons = persons.filter((person) => {
+        const fullName = `${person.firstName} ${person.lastName}`.toLowerCase();
+        console.log(fullName);
+        return fullName.includes(userInput);
+    });
+    console.log(filteredPersons);
 }
