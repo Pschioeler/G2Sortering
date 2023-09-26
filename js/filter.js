fc4aa08a1fd3939d785218b01e54475ae6e7896d
Walocial <walocial@hotmail.com>
2023-09-26 12:10:31 +0200

    xd

--- js/filter.js
+++ js/filter.js
@@ -1,12 +1,42 @@
 const searchBar = document.getElementById('search');
+const dropDown = document.getElementById('dropdown')
 
 searchBar.onkeyup = () => {
     let userInput = searchBar.value.toLowerCase();
-    let filteredPersons = persons.filter((person) => {
-        const fullName = `${person.firstName} ${person.lastName}`.toLowerCase();
-        return fullName.includes(userInput);
-    });
+    let filteredPersons;
+
+   
+
+    if (isNaN(Number(userInput))) { //Tjekker om brugerens input er et nummer
+        dropDown.disabled = true;
+        filteredPersons = persons.filter((person) => {
+            const fullName = `${person.firstName} ${person.lastName}`.toLowerCase();
+            return fullName.includes(userInput);
+        });
+        sortByName(filteredPersons);
+    } else {
+        userInput = Number(userInput);
+        dropDown.disabled = false;
+        filteredPersons = persons.filter((person) => {
+        
+        switch(dropDown.value) {
+            case "1":
+            return person.age === userInput;
+            
+            case "2":
+            return person.age < userInput;
+            
+            case "3":
+            
+            return person.age > userInput;
+        }
+        
+        
+        });
+        sortByAgeDifference(filteredPersons, userInput);
+    }
+
     console.log(filteredPersons);
-    sortByName(filteredPersons);
     print(filteredPersons);
+
 }
